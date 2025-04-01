38234aae68889792c03bd9a48fde44a8e3eb6a39
Juha Tauriainen <juha@bin.fi>
2025-04-01 11:41:49 +0300

    Add sharpjs image manipulation

--- src/api/routes/cat-router.js
+++ src/api/routes/cat-router.js
@@ -6,6 +6,7 @@ import {
   putCat,
 } from '../controllers/cat-controller.js';
 
+import createThumbnail from '../../middlewares.js';
 import express from 'express';
 import multer from 'multer';
 
@@ -13,7 +14,10 @@ const catRouter = express.Router();
 
 const upload = multer({dest: 'uploads/'});
 
-catRouter.route('/').get(getCat).post(upload.single('file'), postCat);
+catRouter
+  .route('/')
+  .get(getCat)
+  .post(upload.single('file'), createThumbnail, postCat);
 
 catRouter.route('/:id').get(getCatById).put(putCat).delete(deleteCat);
 
